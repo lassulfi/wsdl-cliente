b6294daa2fae9e33d9122f45419937794358d723
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-05-10 20:33:33 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/exemplo/wsdlcliente/WsdlClienteApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.wsdlcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdlClienteApplication.class, args);
+	}
+
+}
