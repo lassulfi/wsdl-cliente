67d61f5eac82187d214f81ab5b75b6b3d86784da
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-05-10 22:08:27 -0300

    Cliente criado

--- src/main/java/com/exemplo/wsdlcliente/WsdlClienteApplication.java
+++ src/main/java/com/exemplo/wsdlcliente/WsdlClienteApplication.java
@@ -1,13 +1,35 @@
 package com.exemplo.wsdlcliente;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.exemplo.wsdlcliente.clients.PaisClient;
+import com.exemplo.wsdlcliente.wsdl.GetPaisResponse;
 
 @SpringBootApplication
 public class WsdlClienteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsdlClienteApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner lookup(PaisClient quoteClient) {
+		return args -> {
+			String pais = "Brasil";
+			
+			GetPaisResponse response = quoteClient.getPais(pais);
+			System.out.println(response.getPais().getCapital());
+			System.out.println(response.getPais().getMoeda());
+			System.out.println(response.getPais().getPopulacao());
+			
+			response = quoteClient.getPais("Reino Unido");
+			System.out.println(response.getPais().getCapital());
+			System.out.println(response.getPais().getMoeda());
+			System.out.println(response.getPais().getPopulacao());
+		};
 	}
 
 }
